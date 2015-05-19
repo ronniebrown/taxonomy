f53858310122edb52cf3066cf90ddc9b11d01fb3
Ronnie Brown <ronnievoice@gmail.com>
2015-05-18 23:12:07 -0700

    exercise complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-var Bee = function() {
+var Bee = function(food) {
+  Grub.call(this, food);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
+
 
